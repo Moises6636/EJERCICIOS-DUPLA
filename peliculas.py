b369c4d81cb762e65389893df17a15bc05c23b78
Moises6636 <113399195+Moises6636@users.noreply.github.com>
2023-10-10 14:16:49 -0400

    Create peliculas.py

---
+++ peliculas.py
@@ -0,0 +1,6 @@
+peliculas = ("El señor de los anillos", "Avengers: Endgame", "La La Land")
+horarios = ("16:00", "18:30", "21:00")
+
+print("La película", peliculas[0], "se proyectará a las", horarios[0])
+print("La película", peliculas[1], "se proyectará a las", horarios[1])
+print("La película", peliculas[2], "se proyectará a las", horarios[2])
